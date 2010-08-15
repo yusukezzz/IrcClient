2d847e316ef7b9ad9b5b631c730b3f1430d273da
yusuke <yusuke@114.22.215.131>
2010-08-16 03:05:10 +0900

    コードを整形

--- src/net/yusukezzz/ircviewer/IrcViewer.java
+++ src/net/yusukezzz/ircviewer/IrcViewer.java
@@ -16,98 +16,100 @@ import android.view.Window;
 import android.widget.TextView;
 
 public class IrcViewer extends Activity {
-	private String HOST = "irc.friend-chat.jp";
-	private Integer PORT = 6667;
-	private String NICK = "androzzz";
-	private String LOGIN = "androzzz";
-	private String CHANNEL = "#yusukezzz_test";
+    private String   HOST    = "irc.friend-chat.jp";
+    private Integer  PORT    = 6667;
+    private String   NICK    = "androzzz";
+    private String   LOGIN   = "androzzz";
+    private String   CHANNEL = "#yusukezzz_test";
 
-	private TextView recieve;
-	private Handler handler;
+    private TextView recieve;
+    private Handler  handler;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.viewer);
-		// TexxtViewの準備
-		recieve = (TextView) this.findViewById(R.id.recieve);
-		recieve.setText("irc viewer start...\n");
-		// 受信したテキストをTextViewに出力するhandler
-		this.handler = new Handler() {
-			@Override
-			public void handleMessage(Message msg) {
-				switch (msg.what) {
-				case 0:
-					// 出力
-					recieve.setText(msg.obj.toString() + "\n"
-							+ recieve.getText() + "\n");
-				}
-				super.handleMessage(msg);
-			}
-		};
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.viewer);
+        // TexxtViewの準備
+        recieve = (TextView) this.findViewById(R.id.recieve);
+        recieve.setText("irc viewer start...\n");
+        // 受信したテキストをTextViewに出力するhandler
+        this.handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case 0:
+                        // 出力
+                        recieve.setText(msg.obj.toString() + "\n"
+                                + recieve.getText() + "\n");
+                }
+                super.handleMessage(msg);
+            }
+        };
 
-		// 通信用thread
-		(new Thread() {
-			public void run() {
-				try {
-					connect(HOST, PORT);
-				} catch (Exception e) {
-					// TODO: handle exception
-				}
-			}
-		}).start();
-	}
+        // 通信用thread
+        (new Thread() {
+            public void run() {
+                try {
+                    connect(HOST, PORT);
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
+            }
+        }).start();
+    }
 
-	/**
-	 * IRCサーバに接続し、データを受信する
-	 * @param host
-	 * @param port
-	 */
-	private void connect(String host, Integer port) {
-		try {
-			// 通信開始
-			Socket irc = new Socket(host, port);
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
-					irc.getOutputStream()));
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					irc.getInputStream(), "ISO-2022-JP"));// とりあえず文字コード決め打ち
-			// ログイン
-			bw.write("NICK " + NICK + "\n");
-			bw.write("USER " + LOGIN + " test by yusukezzz\n");
-			bw.flush();
-			// チャンネルに参加
-			bw.write("JOIN " + CHANNEL + "\n");
-			bw.flush();
-			// 1行ずつ処理
-			String current = null;
-			while ((current = br.readLine()) != null) {
-				// PING PONG
-				Pattern pingRegex = Pattern.compile("^PING",
-						Pattern.CASE_INSENSITIVE);
-				Matcher ping = pingRegex.matcher(current);
-				if (ping.find()) {
-					bw.write("PONG " + CHANNEL + "\n");
-					bw.write("PRIVMSG " + CHANNEL + " PONG!\n");
-					bw.flush();
-				}
-				// それ以外のテキストをhandlerへ
-				this.showMessage(current);
-			}
-		} catch (IOException e) {
-			this.showMessage("[Err]" + e.getMessage());
-		}
-	}
-	
-	/**
-	 * handlerに表示するテキストを送る
-	 * @param text
-	 */
-	private void showMessage(String text) {
-		Message msg;
-		msg = new Message();
-		msg.obj = text;
-		msg.what = 0;
-		IrcViewer.this.handler.sendMessage(msg);
-	}
+    /**
+     * IRCサーバに接続し、データを受信する
+     *
+     * @param host
+     * @param port
+     */
+    private void connect(String host, Integer port) {
+        try {
+            // 通信開始
+            Socket irc = new Socket(host, port);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                    irc.getOutputStream()));
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    irc.getInputStream(), "ISO-2022-JP"));// とりあえず文字コード決め打ち
+            // ログイン
+            bw.write("NICK " + NICK + "\n");
+            bw.write("USER " + LOGIN + " test by yusukezzz\n");
+            bw.flush();
+            // チャンネルに参加
+            bw.write("JOIN " + CHANNEL + "\n");
+            bw.flush();
+            // 1行ずつ処理
+            String current = null;
+            while ((current = br.readLine()) != null) {
+                // PING PONG
+                Pattern pingRegex = Pattern.compile("^PING",
+                        Pattern.CASE_INSENSITIVE);
+                Matcher ping = pingRegex.matcher(current);
+                if (ping.find()) {
+                    bw.write("PONG " + CHANNEL + "\n");
+                    bw.write("PRIVMSG " + CHANNEL + " PONG!\n");
+                    bw.flush();
+                }
+                // それ以外のテキストをhandlerへ
+                this.showMessage(current);
+            }
+        } catch (IOException e) {
+            this.showMessage("[Err]" + e.getMessage());
+        }
+    }
+
+    /**
+     * handlerに表示するテキストを送る
+     *
+     * @param text
+     */
+    private void showMessage(String text) {
+        Message msg;
+        msg = new Message();
+        msg.obj = text;
+        msg.what = 0;
+        IrcViewer.this.handler.sendMessage(msg);
+    }
 }
