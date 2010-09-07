c331bee7beddc37eae6309a3046f5a830b964c8e
yusukezzz <g0632198@gmail.com>
2010-09-07 12:56:08 +0900

    shot設定をjsonで保存・読み出しできるように修正

--- src/net/yusukezzz/ircclient/EditHost.java
+++ src/net/yusukezzz/ircclient/EditHost.java
@@ -7,6 +7,7 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -16,46 +17,49 @@ import android.widget.TextView.BufferType;
 
 public class EditHost extends Activity {
     // add host view
-    private EditText hostname;
-    private EditText port;
-    private EditText nick;
-    private EditText login;
-    private String charset;
-    private Button edithostbtn;
+    private EditText  hostname;
+    private EditText  port;
+    private EditText  nick;
+    private EditText  login;
+    private Spinner   charspn;
+    private long      charset;
+    private Button    edithostbtn;
+    private int       host_no = -1;
 
-    private MyJson myjson;
-    private JSONArray json;
-    
-    public EditHost() {
-        this.myjson = new MyJson(this);
-        // 読み込み
-        this.json = myjson.readFile(IrcClient.HOSTS_FILE);
-    }
-    
+    private MyJson    myjson  = null;
+    private JSONArray json    = null;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edithost);
-        
+
+        // host設定読み込み
+        this.myjson = new MyJson(getApplicationContext());
+        this.json = myjson.readFile(IrcClient.HOSTS_FILE);
+
         // 要素の用意
         this.hostname = (EditText) this.findViewById(R.id.edithost_hostname);
         this.port = (EditText) this.findViewById(R.id.edithost_port);
         this.nick = (EditText) this.findViewById(R.id.edithost_nick);
         this.login = (EditText) this.findViewById(R.id.edithost_login);
-        Spinner charspn = (Spinner) this.findViewById(R.id.edithost_charset);
-        this.charset = charspn.getSelectedItem().toString();
+        charspn = (Spinner) this.findViewById(R.id.edithost_charset);
         this.edithostbtn = (Button) this.findViewById(R.id.edithost_btn);
-        
+
         Intent i = getIntent();
-        int host_no = i.getIntExtra("host_no", -1);
-        if (host_no != -1) {
+        host_no = i.getIntExtra("host_no", -1);
+        if (host_no != -1 && !json.isNull(host_no)) {
             try {
                 JSONObject host = json.getJSONObject(host_no);
                 hostname.setText(host.get("name").toString(), BufferType.NORMAL);
+                port.setText(host.get("port").toString(), BufferType.NORMAL);
+                nick.setText(host.get("nick").toString(), BufferType.NORMAL);
+                login.setText(host.get("login").toString(), BufferType.NORMAL);
+                charspn.setSelection(host.getInt("charset"));
             } catch (JSONException e) {
             }
         }
-        
+
         edithostbtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,12 +72,19 @@ public class EditHost extends Activity {
                     new_host.put("port", port.getText());
                     new_host.put("nick", nick.getText());
                     new_host.put("login", login.getText());
+                    charset = charspn.getSelectedItemId();
                     new_host.put("charset", charset);
-                    json.put(new_host);
+                    if (host_no != -1) {
+                        json.put(host_no, new_host);
+                    } else {
+                        json.put(new_host);
+                    }
                     // 書き込み
                     myjson.writeFile(IrcClient.HOSTS_FILE, json.toString());
                 } catch (JSONException e) {
+                    Log.e("yusukezzz", e.getMessage().toString());
                 }
+                myjson = null;
                 setResult(RESULT_OK);
                 finish();
             }
---
+++ src/net/yusukezzz/ircclient/MyJson.java
@@ -0,0 +1,49 @@
+package net.yusukezzz.ircclient;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Context;
+
+public class MyJson {
+    private Context context;
+    
+    public MyJson(Context context) {
+        this.context = context;
+    }
+    
+    public JSONArray readFile(String filename) {
+        JSONArray json = null;
+        try {
+            FileInputStream fis = this.context.openFileInput(filename);
+            byte[] readByte = new byte[fis.available()];
+            fis.read(readByte);
+            json = new JSONArray(new String(readByte));
+        } catch (FileNotFoundException e) {
+            // ファイルがなければ空のJSON
+            return json = new JSONArray();
+        } catch (IOException e) {
+        } catch (JSONException e) {
+        }
+        return json;
+    }
+    
+    public boolean writeFile(String filename, String src) {
+        try {
+            FileOutputStream fos = this.context
+                .openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.write(src.getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            return false;
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+}
